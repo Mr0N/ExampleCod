230122e644e0e1da72930f8c406ec79ddd25db34
new_user <new_user@gmail.com>
2022-01-01 22:34:04 +0200

    Добавьте файлы проекта.

---
+++ ExampleCod/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BufferEnumerable;
+int linqCounter = 0;
+byte[] array = { 0, 0, 1, 0, 1 };
+
+var bytes = array.Where(a =>
+{
+    linqCounter++;
+    return a > 0;
+});
+bool x = bytes.Last() == bytes.First();
+Console.WriteLine(linqCounter);
